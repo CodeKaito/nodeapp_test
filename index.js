3d2500dae11fe9b46c62e15cf81b817b3b966ce8
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-02-07 11:41:42 +0100

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Thetips4you" }');
+    res.send('{ "response": "Hello From CodeKaito" }');
 });
 
 app.get('/will', function (req, res) {
@@ -10,6 +10,18 @@ app.get('/will', function (req, res) {
 });
 app.get('/ready', function (req, res) {
     res.send('{ "response": " Great!, It works!" }');
+});
+app.get('/welcome', function (req, res) {
+    res.send('{ "response": " Welcome! Your Kubernetes cluster is working well" }');
+});
+app.get('/kubernetes', function (req, res) {
+    res.send('{ "response": " I am the test of your k8s" }');
+});
+app.get('/docker', function (req, res) {
+    res.send('{ "response": " I am the test of your docker" }');
+});
+app.get('/nicolas', function (req, res) {
+    res.send('{ "response": " Hello Nic! From k8s" }');
 });
 app.listen(process.env.PORT || 3000);
 module.exports = app;
